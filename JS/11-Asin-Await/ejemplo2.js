43d544df4549c6dfbb1bf1ef4ee5f152fb9d55c2
Jinderarias1303 <sisenornosenor9@gmail.com>
2024-12-06 14:29:47 -0500

    Async Await

---
+++ JS/11-Asin-Await/ejemplo2.js
@@ -0,0 +1,70 @@
+const datos = [
+    {
+        id:1,
+        title: "iron man",
+        year: 2008
+    },
+    {
+        id: 2,
+        title: "spiderman: homecoming",
+        year: 2017
+    },
+    {
+        id: 23,
+        title: "Avenger: Endgame",
+        year: 2019
+    }
+    ]
+    
+    //síncrono
+    const getDatos = ()  => {
+        return datos;
+    }
+    
+    // console.log("INICIO");
+    // console.log(getDatos());
+    // console.log("FIN")
+    
+    //asincrono Callback
+    // const getDatosAsynCallback = (Callback) => {
+    //     setTimeout(function (){
+    //         return Callback();
+    //     }, 3000)
+    // }
+    
+    // console.log("INICIO");
+    // getDatosAsynCallback(getDatos)
+    // console.log("FIN")
+    
+    
+    //promesa
+    // const getDatosPromesa = () =>{
+    //     return new Promise((resolve, reject)=>{
+    //         setTimeout(function (){
+    //             if (datos.length > 0){
+    //                 resolve (datos);
+    //             } else {
+    //                 reject("Error sin datos");
+    //             }
+    //         }, 3000);
+            
+    //     })
+    // };
+    // console.log("INICIO");
+    // getDatosPromesa()
+    //                 .then(datos => console.table(datos))
+    //                 .catch(err => console.error(err));
+    //                 console.log("FIN")
+
+    //Async / Await 
+
+function obtenerDatos() {
+        setTimeout(async()=>{
+            const datos = await getDatos();
+            console.log(datos)
+        },3000)
+    }
+
+    console.log("INICIO");
+    obtenerDatos();
+    console.log("FIN");
